35925c17a66d69087700d1e97b631ac296194cfd
Leonardo Stati <62195020+SaffronBr1440@users.noreply.github.com>
2024-11-27 09:59:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const aviso = document.getElementsByClassName (`botoes_postar`)
+
+function alerta(){aviso.innertext = alert(`sistema indisponivel temporariamente`)};
+
+function alerta2(){aviso.innertext = alert('operação cancelada')}
+
+function alerta3(){aviso.innertext = alert('Formulario enviado com sucesso !!')}
